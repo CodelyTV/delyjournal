1c6a95b67bc8216dd3add4d23b5a00608966c224
Ronny Ancorini <ronnyancorini@gmail.com>
2023-03-23 21:58:25 +0100

    fix: read image by chunks

--- src/app/api/images/upload/route.ts
+++ src/app/api/images/upload/route.ts
@@ -4,9 +4,25 @@ export async function POST(request: Request) {
   const images = Drive("images");
 
   const blob = await request.blob();
-  const result = await blob.stream().getReader().read();
+  const reader = blob.stream().getReader();
+  const buffers: Buffer[] = [];
+  for await (const chunk of readChunks(reader)) {
+    buffers.push(Buffer.from(chunk))
+  }
 
-  images.put("picture2.jpg", {data: result.value});
+  images.put("picture2.jpg", {data: Buffer.concat(buffers)});
 
   return new Response('Hello, Next.js!')
+}
+
+function readChunks(reader: ReadableStreamDefaultReader<Uint8Array>) {
+    return {
+        async* [Symbol.asyncIterator]() {
+            let readResult = await reader.read();
+            while (!readResult.done) {
+                yield readResult.value;
+                readResult = await reader.read();
+            }
+        },
+    };
 }
