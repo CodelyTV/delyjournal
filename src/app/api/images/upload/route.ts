dc1c7fddb3d4521bf98cf25afce63be2d6ac9d9f
Isma Navarro <ismanapa@gmail.com>
2023-04-13 13:37:15 +0200

    style: delete unused code

--- src/app/api/images/upload/route.ts
+++ src/app/api/images/upload/route.ts
@@ -1,29 +1,12 @@
 import {Drive} from "deta";
-import os from "os";
 import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
   const images = Drive("images");
-  const image = (await request.formData()).get("image")
-  const file = await (image as File).arrayBuffer()
+  const image = (await request.formData()).get("image");
+  const file = await (image as File).arrayBuffer();
 
   images.put("image.jpg", {data: Buffer.from(file)});
 
-  return new Response('Hello, Next.js!')
-}
-
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
-}
-
-function readChunks(reader: ReadableStreamDefaultReader<Uint8Array>) {
-    return {
-        async* [Symbol.asyncIterator]() {
-            let readResult = await reader.read();
-            while (!readResult.done) {
-                yield readResult.value;
-                readResult = await reader.read();
-            }
-        },
-    };
+  return new Response('Hello, Next.js!');
 }
